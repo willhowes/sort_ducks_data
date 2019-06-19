e2dcc327bd7c9325c3476fd934d49bb9e3c9df79
eldenhowes <williameldenhowes@gmail.com>
2019-06-19 10:59:57 +0100

    Passes more rpsec tests

--- lib/sort_ducks_data.rb
+++ lib/sort_ducks_data.rb
@@ -1,19 +1,16 @@
 def create_report(names)
   extinct = "Labrador Duck"
   return ["Disqualified data"] if names.any? { |str| str[extinct]}
-  # split each item in array into words and numbers
-  split_names = []
-  names.each {|str| split_names.push(str.split(" "))}
 
-  #set up a Hash
+  split_names = []
+
+  names.each {|str| split_names.push(str.split(/-|\s+/))}
+
   ducks_hash = {}
 
-  # use regex to change the names of the ducks
   split_names.each do |item|
 
     if item.length == 2
-      # If a name has only one word, use the first six letters of the name.
-      # If that name has less than six letters, use what is there.
       name = item[0].upcase
       name = name[/[a-zA-z]{1,6}/]
       if !ducks_hash.include? name
@@ -24,7 +21,6 @@ def create_report(names)
     end
 
     if item.length == 3
-      #If a name has two words, take the first three letters of each word.
       name = item[0][0..2].upcase + item[1][0..2].upcase
       if !ducks_hash.include? name
         ducks_hash[name] = item[2].to_i
@@ -34,17 +30,23 @@ def create_report(names)
     end
 
     if item.length == 4
-      # If a name has three words, take the first two letters of each word.
+      name = item[0][0..1].upcase + item[1][0..1].upcase + item[2][0..1].upcase
+      if !ducks_hash.include? name
+        ducks_hash[name] = item[3].to_i
+      else
+        ducks_hash[name] += item[3].to_i
+      end
     end
 
     if item.length == 5
-      # If a name has four words, take the first letters from the first two words,
-      #  and the first two letters from the last two words.
+      name = item[0][0].upcase + item[1][0].upcase + item[2][0..1].upcase + item[3][0..1].upcase
+      if !ducks_hash.include? name
+        ducks_hash[name] = item[4].to_i
+      else
+        ducks_hash[name] += item[4].to_i
+      end
     end
 
   end
   return ducks_hash.sort.flatten
 end
-
-create_report(["Redhead 3", "Gadwall  1", "Smew 4", "Greater Scaup 10",
-  "Redhead 3", "Gadwall 9", "Greater Scaup  15", "Common Eider 6"])
--- spec/sort_ducks_data_spec.rb
+++ spec/sort_ducks_data_spec.rb
@@ -6,11 +6,20 @@ describe do
     expect(create_report(["Redhead 3", "Gadwall  1", "Smew 4",
       "Greater Scaup 10", "Redhead 3", "Gadwall 9", "Greater Scaup  15",
       "Common Eider 6"])).to eq ["COMEID", 6, "GADWAL", 10,
-        "GRESCA", 25, "REDHEA", 6, "SMEW", 4] 
+        "GRESCA", 25, "REDHEA", 6, "SMEW", 4]
   end
 
   it "Tests for if 'Labrador Duck' is in the list then whole list"\
     "should be thrown out" do
     expect(create_report(["Labrador Duck 3",])).to eq ["Disqualified data"]
   end
+
+  it "Test for a longer list with greater variety of names will be sorted" do
+    expect(create_report(["Black-Bellied Whistling-Duck  5", "Bufflehead  200",
+      "Steller's Eider 1", "Bufflehead  2", "Spectacled Eider   11",
+      "Bufflehead 33", "Eastern Spot-billed Duck 116"
+      ])).to eq ["BBWHDU", 5, "BUFFLE", 235, "ESBIDU", 116,
+        "SPEEID", 11, "STEEID", 1]
+  end
+
 end
