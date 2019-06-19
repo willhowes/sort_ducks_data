6433512d37c6ec2e7fd310037acfba3b7e3b5715
eldenhowes <williameldenhowes@gmail.com>
2019-06-19 11:33:04 +0100

    refactored

--- lib/sort_ducks_data.rb
+++ lib/sort_ducks_data.rb
@@ -1,52 +1,35 @@
 def create_report(names)
-  extinct = "Labrador Duck"
-  return ["Disqualified data"] if names.any? { |str| str[extinct]}
+  return ['Disqualified data'] if names.any? { |str| str['Labrador Duck'] }
 
   split_names = []
+  names.each { |str| split_names.push(str.split(/-|\s+/)) }
 
-  names.each {|str| split_names.push(str.split(/-|\s+/))}
-
-  ducks_hash = {}
+  hash = {}
 
   split_names.each do |item|
-
     if item.length == 2
-      name = item[0].upcase
-      name = name[/[a-zA-z]{1,6}/]
-      if !ducks_hash.include? name
-        ducks_hash[name] = item[1].to_i
-      else
-        ducks_hash[name] += item[1].to_i
-      end
+      name = item[0][/[a-zA-z]{1,6}/].upcase
+      !hash.include?(name) ? hash[name] = item[1].to_i : \
+      hash[name] += item[1].to_i
     end
 
     if item.length == 3
-      name = item[0][0..2].upcase + item[1][0..2].upcase
-      if !ducks_hash.include? name
-        ducks_hash[name] = item[2].to_i
-      else
-        ducks_hash[name] += item[2].to_i
-      end
+      name = (item[0][0..2] + item[1][0..2]).upcase
+      !hash.include?(name)? hash[name] = item[2].to_i : \
+      hash[name] += item[2].to_i
     end
 
     if item.length == 4
-      name = item[0][0..1].upcase + item[1][0..1].upcase + item[2][0..1].upcase
-      if !ducks_hash.include? name
-        ducks_hash[name] = item[3].to_i
-      else
-        ducks_hash[name] += item[3].to_i
-      end
+      name = (item[0][0..1] + item[1][0..1] + item[2][0..1]).upcase
+      !hash.include?(name)? hash[name] = item[3].to_i : \
+      hash[name] += item[3].to_i
     end
 
     if item.length == 5
-      name = item[0][0].upcase + item[1][0].upcase + item[2][0..1].upcase + item[3][0..1].upcase
-      if !ducks_hash.include? name
-        ducks_hash[name] = item[4].to_i
-      else
-        ducks_hash[name] += item[4].to_i
-      end
+      name = (item[0][0] + item[1][0] + item[2][0..1] + item[3][0..1]).upcase
+      !hash.include?(name) ? hash[name] = item[4].to_i : \
+      hash[name] += item[4].to_i
     end
-
   end
-  return ducks_hash.sort.flatten
+  hash.sort.flatten
 end
